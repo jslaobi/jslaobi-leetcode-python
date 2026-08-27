9dce56ee208fbbbec2e103ea35462d88eb268cd8
jslaobi <jslaobi@gmail.com>
2026-08-27 17:33:08 -0400

    Refactor WordSearch to simplify DFS initiation and remove unnecessary comments

--- 0079.WordSearch.py
+++ 0079.WordSearch.py
@@ -1,10 +1,5 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        """DFS 查找单词是否存在于矩阵中。
-
-        时间复杂度: O(m*n*4^L)，m*n 为矩阵大小，L 为单词长度。
-        空间复杂度: O(L)，递归栈深度。
-        """
         rows = len(board)
         cols = len(board[0])
 
@@ -24,9 +19,7 @@ class Solution:
         # 先找到一个首字母相符的格子开始    
         for i in range(rows):
             for j in range(cols):
-                # 这里是dfs(i,j,0)而不是dfs(i,j,1)的原因是,虽然dfs会为0做一个重复检查
-                # 但是我们仍然需要之后的把格子标记成#的步骤,所以为了不重复代码,这里传入0做重复检查是可接受的
-                if board[i][j] == word[0] and dfs(i,j,0):
+                if dfs(i,j,0):
                     return True
 
         return False
